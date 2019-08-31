c31ab9092d3ecef47b0f9d665b365a30cf6a3c6b
mahume <michael.a.hume@gmail.com>
2019-08-31 16:07:55 -0600

    Adding to "add" command

--- app.js
+++ app.js
@@ -2,4 +2,55 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes');
 
+// Customize Version
+yargs.version('1.1.0');
+
+// Add Command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    }
+  },
+  handler({ title, body }) {
+    console.log(`Title: ${title}`);
+    console.log(`Body: ${body}`);
+  }
+})
+// Remove Command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+    console.log('Removing the note');
+    
+  }
+})
+//  List Command
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler() {
+    console.log('Listing all notes');
+  }
+})
+// Read Command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading note');
+  }
+})
+
+
 console.log(yargs.argv);
